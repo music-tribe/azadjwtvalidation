f019b18faf724bed883250065a89ab1a1371efe8
Alex Ford <alex.ford@musictribe.com>
2025-06-05 09:23:19 +0100

    feat: :sparkles: Add azurejwtvalidator.Config to the azurejwtvalidator.AzureJwtValidator and panic if it isn't valid

--- internal/azurejwtvalidator/validator_test.go
+++ internal/azurejwtvalidator/validator_test.go
@@ -8,14 +8,30 @@ import (
 )
 
 func TestNewAzureJwtValidator(t *testing.T) {
+	config := Config{
+		KeysUrl:       "https://jwks.keys",
+		Issuer:        "https://issuer.test",
+		Audience:      "audience1,audience2",
+		Roles:         []string{"Test.Role.1", "Test.Role.2"},
+		MatchAllRoles: true,
+	}
 	t.Run("expect non nil rsakeys", func(t *testing.T) {
-		azureJwtValidator := NewAzureJwtValidator(http.DefaultClient)
+		azureJwtValidator := NewAzureJwtValidator(config, http.DefaultClient)
 		assert.NotNil(t, azureJwtValidator.rsakeys)
 	})
 
 	t.Run("expect our client to be set", func(t *testing.T) {
 		client := http.DefaultClient
-		azureJwtValidator := NewAzureJwtValidator(client)
+		azureJwtValidator := NewAzureJwtValidator(config, client)
 		assert.Equal(t, client, azureJwtValidator.client)
+	})
+
+	t.Run("expect panic if config is invalid", func(t *testing.T) {
+		invalidConfig := Config{
+			KeysUrl: "https://jwks.keys",
+		}
+		assert.Panics(t, func() {
+			NewAzureJwtValidator(invalidConfig, http.DefaultClient)
+		})
 	})
 }
