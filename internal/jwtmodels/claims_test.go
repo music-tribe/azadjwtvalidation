ff312e3990e11a3bd043f63fc3610827be582db8
Alex Ford <alex.ford@musictribe.com>
2025-06-06 14:05:01 +0100

    refactor: :white_check_mark: Add Claims.ValidateRoles with tests

--- internal/jwtmodels/claims_test.go
+++ internal/jwtmodels/claims_test.go
@@ -88,3 +88,124 @@ func TestClaims_IsValidForRole(t *testing.T) {
 		})
 	}
 }
+
+func TestClaims_ValidateRoles(t *testing.T) {
+	l := logger.NewStdLog("warn")
+	type fields struct {
+		Iat   json.Number
+		Exp   json.Number
+		Iss   string
+		Aud   string
+		Sub   string
+		Roles []string
+	}
+	type args struct {
+		configRoles         []string
+		configMatchAllRoles bool
+		l                   logger.Logger
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "expect valid if no config roles",
+			fields: fields{
+				Roles: []string{"Test.Role.1", "Test.Role.2"},
+			},
+			args: args{
+				configRoles:         []string{},
+				configMatchAllRoles: false,
+				l:                   l,
+			},
+			wantErr: false,
+		},
+		{
+			name: "expect valid if we match one config role",
+			fields: fields{
+				Roles: []string{"Test.Role.1"},
+			},
+			args: args{
+				configRoles:         []string{"Test.Role.1"},
+				configMatchAllRoles: false,
+				l:                   l,
+			},
+			wantErr: false,
+		},
+		{
+			name: "expect valid if we match one config role, configMatchAllRoles is false",
+			fields: fields{
+				Roles: []string{"Test.Role.1"},
+			},
+			args: args{
+				configRoles:         []string{"Test.Role.1", "Test.Role.2"},
+				configMatchAllRoles: false,
+				l:                   l,
+			},
+			wantErr: false,
+		},
+		{
+			name: "expect invalid if we match only one config role but configMatchAllRoles is true",
+			fields: fields{
+				Roles: []string{"Test.Role.1"},
+			},
+			args: args{
+				configRoles:         []string{"Test.Role.1", "Test.Role.2"},
+				configMatchAllRoles: true,
+				l:                   l,
+			},
+			wantErr: true,
+		},
+		{
+			name: "expect invalid if we have no roles but we have configRoles set",
+			fields: fields{
+				Roles: []string{},
+			},
+			args: args{
+				configRoles:         []string{"Test.Role.1", "Test.Role.2"},
+				configMatchAllRoles: true,
+				l:                   l,
+			},
+			wantErr: true,
+		},
+		{
+			name: "expect vailid if we have no roles and no configRoles set",
+			fields: fields{
+				Roles: []string{},
+			},
+			args: args{
+				configRoles:         []string{},
+				configMatchAllRoles: true,
+				l:                   l,
+			},
+			wantErr: false,
+		},
+		{
+			name:   "expect invalid if our roles are nil and we have configRoles",
+			fields: fields{},
+			args: args{
+				configRoles:         []string{"Test.Role.1", "Test.Role.2"},
+				configMatchAllRoles: false,
+				l:                   l,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			claims := &Claims{
+				Iat:   tt.fields.Iat,
+				Exp:   tt.fields.Exp,
+				Iss:   tt.fields.Iss,
+				Aud:   tt.fields.Aud,
+				Sub:   tt.fields.Sub,
+				Roles: tt.fields.Roles,
+			}
+			if err := claims.ValidateRoles(tt.args.configRoles, tt.args.configMatchAllRoles, tt.args.l); (err != nil) != tt.wantErr {
+				t.Errorf("Claims.ValidateRoles() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
