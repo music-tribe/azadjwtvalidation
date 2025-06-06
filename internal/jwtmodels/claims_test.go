693c82b2bca28e2168c79528a17932b0db3cbc20
Alex Ford <alex.ford@musictribe.com>
2025-06-06 14:54:29 +0100

    refactor: :white_check_mark: Add Claims.Validate to validate audience, issuer & roles

--- internal/jwtmodels/claims_test.go
+++ internal/jwtmodels/claims_test.go
@@ -209,3 +209,187 @@ func TestClaims_ValidateRoles(t *testing.T) {
 		})
 	}
 }
+
+func TestClaims_Validate(t *testing.T) {
+	l := logger.NewStdLog("warn")
+	type fields struct {
+		Iat   json.Number
+		Exp   json.Number
+		Iss   string
+		Aud   string
+		Sub   string
+		Roles []string
+	}
+	type args struct {
+		audience      string
+		issuer        string
+		allowedRoles  []string
+		matchAllRoles bool
+		l             logger.Logger
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "expect invalid if audience is wrong",
+			fields: fields{
+				Aud:   "test-audience",
+				Iss:   "test-issuer",
+				Roles: []string{"Test.Role.1", "Test.Role.2"},
+			},
+			args: args{
+				audience:      "wrong-audience",
+				issuer:        "test-issuer",
+				allowedRoles:  []string{},
+				matchAllRoles: false,
+				l:             l,
+			},
+			wantErr: true,
+		},
+		{
+			name: "expect invalid if issuer is wrong",
+			fields: fields{
+				Aud:   "test-audience",
+				Iss:   "test-issuer",
+				Roles: []string{"Test.Role.1", "Test.Role.2"},
+			},
+			args: args{
+				audience:      "test-audience",
+				issuer:        "wrong-issuer",
+				allowedRoles:  []string{},
+				matchAllRoles: false,
+				l:             l,
+			},
+			wantErr: true,
+		},
+		{
+			name: "expect valid if no config roles",
+			fields: fields{
+				Aud:   "test-audience",
+				Iss:   "test-issuer",
+				Roles: []string{"Test.Role.1", "Test.Role.2"},
+			},
+			args: args{
+				audience:      "test-audience",
+				issuer:        "test-issuer",
+				allowedRoles:  []string{},
+				matchAllRoles: false,
+				l:             l,
+			},
+			wantErr: false,
+		},
+		{
+			name: "expect valid if we match one config role",
+			fields: fields{
+				Aud:   "test-audience",
+				Iss:   "test-issuer",
+				Roles: []string{"Test.Role.1"},
+			},
+			args: args{
+				audience:      "test-audience",
+				issuer:        "test-issuer",
+				allowedRoles:  []string{"Test.Role.1"},
+				matchAllRoles: false,
+				l:             l,
+			},
+			wantErr: false,
+		},
+		{
+			name: "expect valid if we match one config role, matchAllRoles is false",
+			fields: fields{
+				Aud:   "test-audience",
+				Iss:   "test-issuer",
+				Roles: []string{"Test.Role.1"},
+			},
+			args: args{
+				audience:      "test-audience",
+				issuer:        "test-issuer",
+				allowedRoles:  []string{"Test.Role.1", "Test.Role.2"},
+				matchAllRoles: false,
+				l:             l,
+			},
+			wantErr: false,
+		},
+		{
+			name: "expect invalid if we match only one config role but matchAllRoles is true",
+			fields: fields{
+				Aud:   "test-audience",
+				Iss:   "test-issuer",
+				Roles: []string{"Test.Role.1"},
+			},
+			args: args{
+				audience:      "test-audience",
+				issuer:        "test-issuer",
+				allowedRoles:  []string{"Test.Role.1", "Test.Role.2"},
+				matchAllRoles: true,
+				l:             l,
+			},
+			wantErr: true,
+		},
+		{
+			name: "expect invalid if we have no roles but we have allowedRoles set",
+			fields: fields{
+				Aud:   "test-audience",
+				Iss:   "test-issuer",
+				Roles: []string{},
+			},
+			args: args{
+				audience:      "test-audience",
+				issuer:        "test-issuer",
+				allowedRoles:  []string{"Test.Role.1", "Test.Role.2"},
+				matchAllRoles: true,
+				l:             l,
+			},
+			wantErr: true,
+		},
+		{
+			name: "expect vailid if we have no roles and no allowedRoles set",
+			fields: fields{
+				Aud:   "test-audience",
+				Iss:   "test-issuer",
+				Roles: []string{},
+			},
+			args: args{
+				audience:      "test-audience",
+				issuer:        "test-issuer",
+				allowedRoles:  []string{},
+				matchAllRoles: true,
+				l:             l,
+			},
+			wantErr: false,
+		},
+		{
+			name: "expect invalid if our roles are nil and we have allowedRoles",
+			fields: fields{
+				Aud: "test-audience",
+				Iss: "test-issuer",
+			},
+			args: args{
+				audience:      "test-audience",
+				issuer:        "test-issuer",
+				allowedRoles:  []string{"Test.Role.1", "Test.Role.2"},
+				matchAllRoles: false,
+				l:             l,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			claims := &Claims{
+				Iat:   tt.fields.Iat,
+				Exp:   tt.fields.Exp,
+				Iss:   tt.fields.Iss,
+				Aud:   tt.fields.Aud,
+				Sub:   tt.fields.Sub,
+				Roles: tt.fields.Roles,
+			}
+			if err := claims.Validate(tt.args.audience, tt.args.issuer, tt.args.allowedRoles, tt.args.matchAllRoles, tt.args.l); (err != nil) != tt.wantErr {
+				t.Errorf("Claims.Validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
