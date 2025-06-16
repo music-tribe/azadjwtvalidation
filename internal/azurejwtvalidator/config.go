63e9014db9d4ea0a3c4e107b47628516e4c9c36c
Alex Ford <alex.ford@musictribe.com>
2025-06-16 13:15:06 +0100

    feat: :sparkles: Add UpdateKeysEveryMinutes option to config that determines how often we check for new public keys
    * Default to 24 hours as suggested by Microsoft

--- internal/azurejwtvalidator/config.go
+++ internal/azurejwtvalidator/config.go
@@ -3,15 +3,19 @@ package azurejwtvalidator
 import "github.com/go-playground/validator/v10"
 
 type Config struct {
-	PublicKey     string `validate:"required_without=KeysUrl"`
-	KeysUrl       string `validate:"required_without=PublicKey,omitempty,http_url"`
-	Issuer        string `validate:"required,http_url"`
-	Audience      string `validate:"required"`
-	Roles         []string
-	MatchAllRoles bool
+	PublicKey              string `validate:"required_without=KeysUrl"`
+	KeysUrl                string `validate:"required_without=PublicKey,omitempty,http_url"`
+	Issuer                 string `validate:"required,http_url"`
+	Audience               string `validate:"required"`
+	Roles                  []string
+	MatchAllRoles          bool
+	UpdateKeysEveryMinutes int `validate:"required,min=5,max=2880"` // 2 days in minutes, Reasonable frequency is 24 hours: https://learn.microsoft.com/en-us/azure/active-directory-b2c/tokens-overview#validate-signature
 }
 
 func (c *Config) validate() error {
+	if c.UpdateKeysEveryMinutes == 0 {
+		c.UpdateKeysEveryMinutes = 1440 // Default to 24 hours
+	}
 	validate := validator.New(validator.WithRequiredStructEnabled())
 	err := validate.Struct(c)
 	if err != nil {
--- internal/azurejwtvalidator/config_test.go
+++ internal/azurejwtvalidator/config_test.go
@@ -51,4 +51,36 @@ func TestConfig_validate(t *testing.T) {
 		err := config.validate()
 		assert.NoError(t, err)
 	})
+	t.Run("expect UpdateKeysEveryMinutes to be invalid if less than 5", func(t *testing.T) {
+		config := Config{
+			PublicKey:              "some public-key",
+			Issuer:                 "https://issuer.test",
+			Audience:               "audience1",
+			UpdateKeysEveryMinutes: 4,
+		}
+		err := config.validate()
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "Error:Field validation for 'UpdateKeysEveryMinutes' failed on the 'min' tag")
+	})
+	t.Run("expect UpdateKeysEveryMinutes to be invalid if more than 2880", func(t *testing.T) {
+		config := Config{
+			PublicKey:              "some public-key",
+			Issuer:                 "https://issuer.test",
+			Audience:               "audience1",
+			UpdateKeysEveryMinutes: 2881,
+		}
+		err := config.validate()
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "Error:Field validation for 'UpdateKeysEveryMinutes' failed on the 'max' tag")
+	})
+	t.Run("expect UpdateKeysEveryMinutes to default to 1440 minutes in not provided", func(t *testing.T) {
+		config := Config{
+			PublicKey: "some public-key",
+			Issuer:    "https://issuer.test",
+			Audience:  "audience1",
+		}
+		err := config.validate()
+		assert.NoError(t, err)
+		assert.Equal(t, 1440, config.UpdateKeysEveryMinutes, "UpdateKeysEveryMinutes should default to 1440 minutes (24 hours)")
+	})
 }
