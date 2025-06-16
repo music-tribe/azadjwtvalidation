c75101ad5261203d02f65c1dd4f0934c8914a348
Alex Ford <alex.ford@musictribe.com>
2025-06-16 21:36:37 +0100

    feat: :white_check_mark: Add backoff retry to ScheduleUpdateKeys to retry GetPublicKeys if it fails

--- internal/azurejwtvalidator/config.go
+++ internal/azurejwtvalidator/config.go
@@ -3,13 +3,14 @@ package azurejwtvalidator
 import "github.com/go-playground/validator/v10"
 
 type Config struct {
-	PublicKey              string `validate:"required_without=KeysUrl"`
-	KeysUrl                string `validate:"required_without=PublicKey,omitempty,http_url"`
-	Issuer                 string `validate:"required,http_url"`
-	Audience               string `validate:"required"`
-	Roles                  []string
-	MatchAllRoles          bool
-	UpdateKeysEveryMinutes int `validate:"required,min=5,max=2880"` // 2 days in minutes, Reasonable frequency is 24 hours: https://learn.microsoft.com/en-us/azure/active-directory-b2c/tokens-overview#validate-signature
+	PublicKey                    string `validate:"required_without=KeysUrl"`
+	KeysUrl                      string `validate:"required_without=PublicKey,omitempty,http_url"`
+	Issuer                       string `validate:"required,http_url"`
+	Audience                     string `validate:"required"`
+	Roles                        []string
+	MatchAllRoles                bool
+	UpdateKeysEveryMinutes       int    `validate:"required,min=5,max=2880"` // 2 days in minutes, Reasonable frequency is 24 hours: https://learn.microsoft.com/en-us/azure/active-directory-b2c/tokens-overview#validate-signature
+	UpdateKeysWithBackoffRetries uint64 // Number of retries for backoff when fetching keys. Default 0 is no retries.
 }
 
 func (c *Config) validate() error {
--- internal/azurejwtvalidator/scheduleupdatekeys_test.go
+++ internal/azurejwtvalidator/scheduleupdatekeys_test.go
@@ -111,3 +111,34 @@ func TestAzureJwtValidator_ScheduleUpdateKeys(t *testing.T) {
 		assert.Empty(t, azjwt.rsakeys)
 	})
 }
+
+func TestAzureJwtValidator_getPublicKeysWithBackoffRetry(t *testing.T) {
+	t.Run("expect to fail and retry a few times", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		ml := logger.NewMockLogger(ctrl)
+
+		azjwt := &AzureJwtValidator{
+			config: Config{
+				KeysUrl:                "https://login.microsoftonline.com/common/discovery/v2.0/keys",
+				Audience:               "test-audience",
+				Issuer:                 "https://login.microsoftonline.com/9188040d-6c67-4c5b-b112-36a304b66dad/v2.0",
+				Roles:                  []string{"Test.Role.1", "Test.Role.2"},
+				UpdateKeysEveryMinutes: 1,
+			},
+			client: &http.Client{
+				Transport: newStubRoundTripper(
+					&http.Response{
+						StatusCode: http.StatusServiceUnavailable,
+					},
+					nil),
+			},
+			logger: ml,
+		}
+
+		ml.EXPECT().Warn("failed to retrieve keys. Response: , Body: ").Times(3)
+
+		err := azjwt.getPublicKeysWithBackoffRetry(2)
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "failed to retrieve keys. Response: , Body: ")
+	})
+}
