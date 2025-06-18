18263fa56fa3334fe31ea767575ea049b51a1e8e
Alex Ford <alex.ford@musictribe.com>
2025-06-18 15:49:52 +0100

    fix: :fire: Remove go-playground/validator/v10 as we can't run this in yaegi due to it importing syscall
    * syscall is not allowed in yaegi which is the Go interpreter Traefik uses to run the plugins
    * azurejwtvalidator.Config is now validated manually

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ toolchain go1.24.3
 
 require (
 	github.com/cenkalti/backoff/v5 v5.0.2
-	github.com/go-playground/validator/v10 v10.26.0
 	github.com/golang-jwt/jwt/v4 v4.4.2
 	github.com/stretchr/testify v1.9.0
 	go.uber.org/mock v0.5.2
@@ -14,14 +13,6 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
-	github.com/go-playground/locales v0.14.1 // indirect
-	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/crypto v0.33.0 // indirect
-	golang.org/x/net v0.34.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
-	golang.org/x/text v0.22.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/azurejwtvalidator/config.go
+++ internal/azurejwtvalidator/config.go
@@ -1,15 +1,18 @@
 package azurejwtvalidator
 
-import "github.com/go-playground/validator/v10"
+import "fmt"
 
+// Configuration for the Azure JWT Validator.
+// Ideally, we would use go-playground/validator/v10 to validate the configuration,
+// but this package imports syscall which is not allowed in Yaegi used by Traefik.
 type Config struct {
-	PublicKey                    string `validate:"required_without=KeysUrl"`
-	KeysUrl                      string `validate:"required_without=PublicKey,omitempty,http_url"`
-	Issuer                       string `validate:"required,http_url"`
-	Audience                     string `validate:"required"`
+	PublicKey                    string // `validate:"required_without=KeysUrl"`
+	KeysUrl                      string // `validate:"required_without=PublicKey,omitempty,http_url"`
+	Issuer                       string // `validate:"required,http_url"`
+	Audience                     string // `validate:"required"`
 	Roles                        []string
 	MatchAllRoles                bool
-	UpdateKeysEveryMinutes       int  `validate:"required,min=5,max=2880"` // 2 days in minutes, Reasonable frequency is 24 hours: https://learn.microsoft.com/en-us/azure/active-directory-b2c/tokens-overview#validate-signature
+	UpdateKeysEveryMinutes       int  // `validate:"required,min=5,max=2880"` // 2 days in minutes, Reasonable frequency is 24 hours: https://learn.microsoft.com/en-us/azure/active-directory-b2c/tokens-overview#validate-signature
 	UpdateKeysWithBackoffRetries uint // Number of retries for backoff when fetching keys. Default 0 is no retries.
 }
 
@@ -17,10 +20,21 @@ func (c *Config) validate() error {
 	if c.UpdateKeysEveryMinutes == 0 {
 		c.UpdateKeysEveryMinutes = 1440 // Default to 24 hours
 	}
-	validate := validator.New(validator.WithRequiredStructEnabled())
-	err := validate.Struct(c)
-	if err != nil {
-		return err
+	if c.PublicKey == "" && c.KeysUrl == "" {
+		return fmt.Errorf("either PublicKey or KeysUrl must be provided")
+	}
+	if c.Issuer == "" {
+		return fmt.Errorf("Error:Field validation for 'Issuer' failed on the 'required' tag")
+	}
+	if c.Audience == "" {
+		return fmt.Errorf("Error:Field validation for 'Audience' failed on the 'required' tag")
+	}
+	if c.UpdateKeysEveryMinutes < 5 {
+		return fmt.Errorf("Error:Field validation for 'UpdateKeysEveryMinutes' failed on the 'min' tag")
+	}
+
+	if c.UpdateKeysEveryMinutes > 2880 {
+		return fmt.Errorf("Error:Field validation for 'UpdateKeysEveryMinutes' failed on the 'max' tags")
 	}
 	return nil
 }
